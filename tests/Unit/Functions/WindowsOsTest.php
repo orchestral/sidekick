ed4e3764c0f68895594b36f7ffb69cff2bdb7afa
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-05 06:58:05 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Functions/WindowsOsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Orchestra\Sidekick\Tests\Unit\Functions;
+
+use PHPUnit\Framework\TestCase;
+
+use function windows_os as laravel_windows_os;
+use function Orchestra\Sidekick\windows_os;
+
+class WindowsOsTest extends TestCase
+{
+    public function test_it_return_false_when_path_is_not_a_symlink()
+    {
+        $this->assertSame(laravel_windows_os(), windows_os());
+    }
+}
