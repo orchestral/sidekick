0cdf599c831f3f7289fa711541e157ac4794059a
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-15 05:50:29 +0800

    Add Eloquent helper functions.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Functions/Eloquent/ModelExistsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Orchestra\Sidekick\Tests\Functions\Eloquent;
+
+use Illuminate\Foundation\Auth\User;
+use PHPUnit\Framework\TestCase;
+
+use function Orchestra\Sidekick\Eloquent\model_exists;
+
+class ModelExistsTest extends TestCase
+{
+    public function test_it_can_detect_existing_model()
+    {
+        $user = new User();
+        $user->exists = true;
+
+        $this->assertTrue(model_exists($user));
+    }
+
+    public function test_it_can_detect_none_existing_model()
+    {
+        $user = new User();
+
+        $this->assertFalse(model_exists($user));
+    }
+}
