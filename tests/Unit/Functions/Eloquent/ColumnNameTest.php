df1d1976a44a45cde500530d66e243ff686c56c7
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-15 05:57:41 +0800

    Add Eloquent helper functions. (#12)
    * Add Eloquent helper functions.
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Functions/Eloquent/ColumnNameTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Orchestra\Sidekick\Tests\Functions\Eloquent;
+
+use Illuminate\Foundation\Auth\User;
+use PHPUnit\Framework\TestCase;
+
+use function Orchestra\Sidekick\Eloquent\column_name;
+
+class ColumnNameTest extends TestCase
+{
+    public function test_it_can_translate_column_name()
+    {
+        $column = column_name(User::class, 'email');
+
+        $this->assertSame('users.email', $column);
+    }
+
+    public function test_it_can_translate_column_name_when_given_an_instance_of_eloquent()
+    {
+        $column = column_name(new User, 'email');
+
+        $this->assertSame('users.email', $column);
+    }
+
+    public function test_it_cant_translate_column_name_when_not_given_an_instance_of_eloquent()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->expectExceptionMessage('Given $model is not an instance of [Illuminate\Database\Eloquent\Model].');
+
+        $column = column_name(new class
+        {
+            //
+        }, 'email');
+    }
+}
---
+++ tests/Unit/Functions/Eloquent/TableNameTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Orchestra\Sidekick\Tests\Functions\Eloquent;
+
+use Illuminate\Foundation\Auth\User;
+use PHPUnit\Framework\TestCase;
+
+use function Orchestra\Sidekick\Eloquent\table_name;
+
+class TableNameTest extends TestCase
+{
+    public function test_it_can_translate_table_name()
+    {
+        $table = table_name(User::class);
+
+        $this->assertSame('users', $table);
+    }
+
+    public function test_it_can_translate_table_name_when_given_an_instance_of_eloquent()
+    {
+        $table = table_name(new User);
+
+        $this->assertSame('users', $table);
+    }
+
+    public function test_it_cant_translate_table_name_when_not_given_an_instance_of_eloquent()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->expectExceptionMessage('Given $model is not an instance of [Illuminate\Database\Eloquent\Model].');
+
+        $table = table_name(new class
+        {
+            //
+        });
+    }
+}
