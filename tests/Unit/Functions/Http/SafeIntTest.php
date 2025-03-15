33c6b297761a1b2bd398e99245bf85e17773723b
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-16 00:02:42 +0800

    Add `Orchestra\Sidekick\Http\safe_int`
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Functions/Http/SafeIntTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Orchestra\Sidekick\Tests\Unit\Functions\Http;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+use function Orchestra\Sidekick\Http\safe_int;
+
+class SafeIntTest extends TestCase
+{
+    /**
+     * @dataProvider castSafeIntDataProvider
+     */
+    #[DataProvider('castSafeIntDataProvider')]
+    public function test_it_can_cast_safe_int(int|string $given, int|string $expected)
+    {
+        $this->assertSame($expected, safe_int($given));
+    }
+
+    public static function castSafeIntDataProvider()
+    {
+        yield [1, 1];
+        yield ['foo', 'foo'];
+        yield [9007199254740990, 9007199254740990];
+        yield ['9007199254740990', 9007199254740990];
+        yield [9007199254740991, '9007199254740991'];
+        yield ['9007199254740991', '9007199254740991'];
+        yield [9007199254741001, '9007199254741001'];
+        yield [-9007199254741001, '-9007199254741001'];
+    }
+}
