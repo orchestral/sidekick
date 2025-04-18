f3007f613603f2e5ce6793cd34e1bb8b8c09c7a4
Mior Muhammad Zaki <crynobone@gmail.com>
2025-04-18 08:28:48 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Php81/Functions/Enums.php
+++ tests/Php81/Functions/Enums.php
@@ -2,23 +2,21 @@
 
 namespace Orchestra\Sidekick\Tests\Php81\Functions;
 
-if (PHP_VERSION_ID < 80100) {
-    return;
-}
+if (PHP_VERSION_ID >= 80100) {
+    enum TestEnum
+    {
+        case A;
+    }
 
-enum TestEnum
-{
-    case A;
-}
+    enum TestBackedEnum: int
+    {
+        case A = 1;
+        case B = 2;
+    }
 
-enum TestBackedEnum: int
-{
-    case A = 1;
-    case B = 2;
-}
-
-enum TestStringBackedEnum: string
-{
-    case A = 'A';
-    case B = 'B';
+    enum TestStringBackedEnum: string
+    {
+        case A = 'A';
+        case B = 'B';
+    }
 }
