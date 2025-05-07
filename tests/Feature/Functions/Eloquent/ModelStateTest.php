e05b4941b67ef85ddc21d4a1affd2a250bafb8d3
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-07 23:43:37 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/Functions/Eloquent/ModelStateTest.php
+++ tests/Feature/Functions/Eloquent/ModelStateTest.php
@@ -19,7 +19,7 @@ class ModelStateTest extends TestCase
         //
     }
 
-    public function test_it_can_validate_changes_on_creating_a_model()
+    public function test_it_can_detect_changes_on_creating_a_model()
     {
         $user = (new User)->forceFill([
             'name' => 'Mior Muhammad Zaki',
@@ -30,8 +30,31 @@ class ModelStateTest extends TestCase
         [$original, $changes] = model_state($user);
 
         $this->assertNull($original);
+        $this->assertSame(['name', 'email', 'password'], array_keys($changes));
         $this->assertSame('Mior Muhammad Zaki', $changes['name']);
         $this->assertSame('crynobone@gmail.com', $changes['email']);
+        $this->assertInstanceOf(SensitiveValue::class, $changes['password']);
+    }
+
+    public function test_it_can_detect_changes_on_updating_a_model()
+    {
+        $user = (new User([
+            'name' => 'Mior Muhammad Zaki',
+            'email' => 'crynobone@gmail.com',
+            'password' => $password = password_hash('secret', PASSWORD_DEFAULT),
+        ]));
+
+        $user->syncOriginal();
+
+        $user->exists = true;
+        $user->name = 'Mior Muhammad Zaki bin Mior Khairuddin';
+        $user->password = password_hash('password', PASSWORD_DEFAULT);
+
+        [$original, $changes] = model_state($user);
+
+        $this->assertSame(['name' => 'Mior Muhammad Zaki'], $original);
+        $this->assertSame(['name', 'password'], array_keys($changes));
+        $this->assertSame('Mior Muhammad Zaki bin Mior Khairuddin', $changes['name']);
         $this->assertInstanceOf(SensitiveValue::class, $changes['password']);
     }
 }
