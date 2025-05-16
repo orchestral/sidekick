2b714d6ea10cbefbef073650c9dab2508592cf8a
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-16 11:17:28 +0800

    Add `Orchestra\Sidekick\Eloquent\Concerns\HasPreviousAttributes` trait (#42)
    * Add `Orchestra\Sidekick\Eloquent\Concerns\HasPreviousAttributes` trait.
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Eloquent/Concerns/HasPreviousAttributes.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Orchestra\Sidekick\Eloquent\Concerns;
+
+/**
+ * Polyfill for Eloquent Model to get previous attributes.
+ *
+ * @see https://github.com/laravel/framework/pull/55729
+ */
+trait HasPreviousAttributes
+{
+    /**
+     * The previous state of the changed model attributes.
+     *
+     * @var array<string, mixed>
+     */
+    protected $previous = [];
+
+    /** {@inheritDoc} */
+    #[\Override]
+    public function syncChanges()
+    {
+        parent::syncChanges();
+
+        $this->previous = array_intersect_key($this->getRawOriginal(), $this->changes);
+
+        return $this;
+    }
+
+    /** {@inheritDoc} */
+    #[\Override]
+    public function discardChanges()
+    {
+        $this->previous = [];
+
+        return parent::discardChanges();
+    }
+
+    /**
+     * Get the attributes that were previously original before the model was last saved.
+     *
+     * @return array<string, mixed>
+     */
+    public function getPrevious()
+    {
+        return $this->previous;
+    }
+}
