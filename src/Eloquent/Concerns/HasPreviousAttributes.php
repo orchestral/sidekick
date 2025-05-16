3d4f692259b47266f7b7eaf7beb69c319c12b773
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-16 10:56:39 +0800

    Add `Orchestra\Sidekick\Eloquent\Concerns\HasPreviousAttributes` trait.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Eloquent/Concerns/HasPreviousAttributes.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Orchestra\Sidekick\Eloquent\Concerns;
+
+use Orchestra\Sidekick\Eloquent\Watcher;
+
+/**
+ * Polyfill for Eloquent Model to get previous attributes.
+ *
+ * @see https://github.com/laravel/framework/pull/55729
+ */
+trait HasPreviousAttributes
+{
+    /**
+     * The previous state of the changed model attributes.
+     *
+     * @var array
+     */
+    protected $previous = [];
+
+    /** {@inheritdoc} */
+    #[\Override]
+    public function syncChanges()
+    {
+        parent::syncChanges();
+
+        $this->previous = array_intersect_key($this->getRawOriginal(), $this->changes);
+
+        /** @phpstan-ignore offsetAssign.valueType */
+        Watcher::store()[$this] = $this->previous;
+
+        return $this;
+    }
+
+    /** {@inheritdoc} */
+    #[\Override]
+    public function discardChanges()
+    {
+        $this->previous = [];
+
+        return parent::discardChanges();
+    }
+
+    /**
+     * Get the attributes that were previously original before the model was last saved.
+     *
+     * @return array
+     */
+    public function getPrevious()
+    {
+        return $this->previous;
+    }
+}
