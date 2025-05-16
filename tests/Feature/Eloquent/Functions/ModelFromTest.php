86b0c19fc42275879865f2d18804dd102120362a
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-17 07:05:14 +0800

    Add `Orchestra\Sidekick\Eloquent\model_from()` function (#44)
    * Add `Orchestra\Sidekick\Eloquent\model_from()` function
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Update ModelFromTest.php
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/Eloquent/Functions/ModelFromTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Orchestra\Sidekick\Tests\Feature\Eloquent\Functions;
+
+use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Testbench\Attributes\WithConfig;
+use Orchestra\Testbench\TestCase;
+
+use function Orchestra\Sidekick\Eloquent\model_from;
+
+#[WithConfig('database.default', 'testing')]
+class ModelFromTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_can_resolve_a_model_from_attributes()
+    {
+        $now = CarbonImmutable::now();
+
+        $user = model_from(User::class, ['name' => 'Mior Muhammad Zaki', 'email_verified_at' => $now]);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertFalse($user->exists);
+        $this->assertSame('Mior Muhammad Zaki', $user->name);
+        $this->assertInstanceOf('Illuminate\Support\Carbon', $user->email_verified_at);
+        $this->assertSame($now->toJSON(), $user->email_verified_at->toJSON());
+    }
+}
