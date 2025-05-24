9bd487f30862a956f176fc26544aa96586587f6d
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-24 23:03:13 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Env.php
+++ src/Env.php
@@ -50,8 +50,12 @@ class Env extends \Illuminate\Support\Env
      * @param  \Orchestra\Sidekick\UndefinedValue|mixed|null  $default
      * @return mixed
      */
-    public static function forward(string $key, $default = new UndefinedValue)
+    public static function forward(string $key, $default = null)
     {
+        if (\func_num_args() === 1) {
+            $default = new UndefinedValue;
+        }
+
         $value = static::get($key, $default);
 
         if ($value instanceof UndefinedValue) {
