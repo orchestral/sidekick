b31dea1f03da7a398ce57f0a7d1030cfaea398b3
Mior Muhammad Zaki <crynobone@gmail.com>
2025-08-06 21:11:24 +0800

    Supports PHPUnit 12.4 (dev)
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/functions.php
+++ src/functions.php
@@ -174,7 +174,7 @@ if (! \function_exists('Orchestra\Sidekick\phpunit_version_compare')) {
         $phpunit = transform(
             Version::id(),
             fn (string $version) => match (true) {
-                str_starts_with($version, '12.3-') => '12.3.0',
+                str_starts_with($version, '12.4-') => '12.4.0',
                 default => $version,
             }
         );
