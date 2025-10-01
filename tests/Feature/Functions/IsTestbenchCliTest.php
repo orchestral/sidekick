838304aa7aa5b875bfd0ab0e41e82560a5e457a2
Mior Muhammad Zaki <crynobone@gmail.com>
2025-10-01 19:28:53 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/Functions/IsTestbenchCliTest.php
+++ tests/Feature/Functions/IsTestbenchCliTest.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Sidekick\Tests\Feature\Functions;
 
+use Orchestra\Testbench\Attributes\RequiresLaravel;
 use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
 
@@ -11,6 +12,7 @@ use function Orchestra\Testbench\remote;
 #[WithConfig('app.key', 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF')]
 class IsTestbenchCliTest extends TestCase
 {
+    #[RequiresLaravel('>=11.44.7')]
     public function test_it_can_detect_testbench_environment()
     {
         $process = remote(fn () => is_testbench_cli());
