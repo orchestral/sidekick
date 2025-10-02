a835e9093daf6f9dbfe48fff8777a129bcc78645
Mior Muhammad Zaki <crynobone@gmail.com>
2025-10-02 12:57:58 +0800

    Allows `Orchestra\Sidekick\is_testbench_cli()` to detect running via Testbench Dusk CLI (#56)
    * Allows `Orchestra\Sidekick\is_testbench_cli()` to detect running via
    Testbench Dusk CLI
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/Functions/IsTestbenchCliTest.php
+++ tests/Feature/Functions/IsTestbenchCliTest.php
@@ -5,20 +5,36 @@ namespace Orchestra\Sidekick\Tests\Feature\Functions;
 use Orchestra\Testbench\Attributes\RequiresLaravel;
 use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\CoversFunction;
 
 use function Orchestra\Sidekick\is_testbench_cli;
 use function Orchestra\Testbench\remote;
 
 #[WithConfig('app.key', 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF')]
+#[CoversFunction('\Orchestra\Sidekick\is_testbench_cli')]
 class IsTestbenchCliTest extends TestCase
 {
     #[RequiresLaravel('>=11.44.7')]
     public function test_it_can_detect_testbench_environment()
     {
-        $process = remote(fn () => is_testbench_cli());
-        $result = $process->mustRun();
-
         $this->assertFalse(is_testbench_cli());
-        $this->assertTrue($result->output());
+
+        $this->assertTrue(transform(remote(fn () => is_testbench_cli()), function ($process) {
+            $result = $process->mustRun();
+
+            return $result->output();
+        }));
+
+        $this->assertTrue(transform(remote(fn () => is_testbench_cli(dusk: false)), function ($process) {
+            $result = $process->mustRun();
+
+            return $result->output();
+        }));
+
+        $this->assertFalse(transform(remote(fn () => is_testbench_cli(dusk: true)), function ($process) {
+            $result = $process->mustRun();
+
+            return $result->output();
+        }));
     }
 }
