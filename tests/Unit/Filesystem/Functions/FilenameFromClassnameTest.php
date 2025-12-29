0827c4aa70d5bd914bf6024af109c48dd5c83ecc
Mior Muhammad Zaki <crynobone@gmail.com>
2025-12-29 22:43:23 +0800

    Add `Orchestra\Sidekick\Filesystem\filename_from_classname()` function (#59)
    * Add `Orchestra\Sidekick\Filesystem\filename_from_classname()` function
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * refactor
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * add tests
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/Filesystem/Functions/FilenameFromClassnameTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Orchestra\Sidekick\Tests\Unit\Filesystem\Functions;
+
+use PHPUnit\Framework\TestCase;
+
+use function Orchestra\Sidekick\Filesystem\filename_from_classname;
+use function Orchestra\Sidekick\join_paths;
+
+class FilenameFromClassnameTest extends TestCase
+{
+    /**
+     * The `orchestra/sidekick` directory.
+     */
+    protected string $projectDirectory;
+
+    /** {@inheritDoc} */
+    #[\Override]
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->projectDirectory = join_paths(
+            realpath(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..')
+        );
+    }
+
+    /** {@inheritDoc} */
+    #[\Override]
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        unset($this->projectDirectory);
+    }
+
+    public function test_it_can_resolve_classes()
+    {
+        $this->assertSame(
+            join_paths($this->projectDirectory, 'src', 'UndefinedValue.php'),
+            filename_from_classname('Orchestra\Sidekick\UndefinedValue')
+        );
+
+        $this->assertFalse(filename_from_classname('Orchestra\Sidekick\DefinedValue'));
+    }
+}
