e317da131d08a9218ceea04d24c01b66ee1caa7a
Mior Muhammad Zaki <crynobone@gmail.com>
2026-09-23 21:06:45 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/functions.php
+++ src/functions.php
@@ -267,7 +267,7 @@ if (! \function_exists('Orchestra\Sidekick\php_normalize_version')) {
     {
         /** @var string $version */
         $version = match (true) {
-            str_ends_with('-dev', PHP_VERSION) && PHP_VERSION_ID === 80600 => '8.6.0', // @phpstan-ignore booleanAnd.alwaysFalse,identical.alwaysFalse
+            str_ends_with('-dev', PHP_VERSION) && PHP_VERSION_ID === 80600 => '8.6.0',
             default => phpversion(),
         };
 
